0f621edc6cc4982b8b412a1634fcc854868de7f8
Mathías Montossi <mmontossi@mattways.com>
2012-09-04 16:45:09 -0300

    Automatic selection of locale by subdomain or prefix

---
+++ lib/rails_i18n_routes/action_controller.rb
@@ -0,0 +1,28 @@
+module RailsI18nRoutes
+  module ActionController
+    module BaseMethods
+      
+      def self.included?(base)
+        base.send :prepend_before_filter :detect_locale
+      end
+      
+      def select_locale
+        if Rails.application.i18n_routes.selection == :subdomain
+          Rails.application.i18n_routes.mapping.each_pair do |key, value|
+            if value.include? request.subdomain
+              I18n.locale = "#{key}_#{request.subdomain}" 
+              break
+            end
+          end
+        else
+          unless params[:locale].nil?
+            I18n.locale = params[:locale]
+          end
+        end        
+      end
+      
+    end
+  end
+end
+
+ActionController::Base.send :include, RailsI18nRoutes::ActionController::BaseMethods
